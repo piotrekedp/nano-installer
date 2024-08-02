732d71052e8b9290948ddd4d4150b59cb5246786
Piotrek <markowskip@proton.me>
2024-08-02 23:33:46 +0200

    sprawdzanie ncurses

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "../include/installer.h"
+
+int check_ncurses_header();
 
 int main() {
     printf("Rozpoczynam instalację edytora nano\n");
     
-    // Tu będzie logika instalacji
-    
+    if(check_ncurses_header()==1){
+      printf("mamy ncurses\n");
+    }
+    else {
+      printf("brak ncurses\n");
+    }
     return 0;
 }
 
+int check_ncurses_header() {
+    struct stat buffer;
+    return stat("/usr/include/ncurses.h", &buffer) == 0;
+}
